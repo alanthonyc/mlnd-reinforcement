8ebaba7c14cf3fa0e0cc775ceab72002c3396f57
Anthony Castillo <anthony@anthonycas.com>
2017-11-29 12:32:57 -0800

    Post Q5 - Update the Driving Agent State
    * assign a `state` value in the `build_state()` function
        - based on answer to Q4
            - `waypoint`
            - `inputs / left`
            - `inputs / oncoming`
            - `inputs / light`
        - `inputs / right` is ignored

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -62,7 +62,7 @@ class LearningAgent(Agent):
         # With the hand-engineered features, this learning process gets entirely negated.
         
         # Set 'state' as a tuple of relevant data for the agent        
-        state = None
+        state = (waypoint, inputs['oncoming'], inputs['left'], inputs['right'])
 
         return state
 
